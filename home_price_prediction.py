79b448fd0e63770589aaafcefc13b0dc4da4b173
kumarGN69 <kumargn1969@otmail.com>
2025-03-06 11:54:08 -0600

    Work in progress for ad budget

--- home_price_prediction.py
+++ home_price_prediction.py
@@ -1,84 +1,60 @@
 import pandas as pd
-import numpy as np 
 import matplotlib
 import matplotlib.pyplot as plot
-import seaborn as sns 
-import dotenv , os
 
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score, root_mean_squared_error
-from sklearn.preprocessing import StandardScaler, PolynomialFeatures
 
 
+def plot_3d_pred_homeprice(x,y,z):
+    """
+        :param x: Area of the home
+        :param y: Number of bedrooms
+        :param z: The predicted value of the home
+        :return: None - No return value
+    """
+    fig = plot.figure()
+    ax = plot.axes(projection='3d')
+    ax.scatter(x,y,z)
+    ax.set_title(label='3D plot Home Price Prediction')
+    ax.set_xlabel('Area',fontsize=10)
+    ax.set_ylabel('BedRooms',fontsize=10)
+    ax.set_zlabel('Price',fontsize=10)
+    plot.savefig('3dplt_pred_output.png')
 
-dotenv.load_dotenv()
-matplotlib.use('Agg')
+if __name__ == "__main__":
 
-df = pd.read_csv('./home.csv',names=['Area','BedRooms','Price'])
-# print(df)
+    #read the csv file into a dataframe
+    df = pd.read_csv('./home.csv',names=['Area','BedRooms','Price'])
 
-# sns.pairplot(
-#     data=df
-# )
-# plot.savefig('plt.png')
+    #extract the relevant independent and dependent features
+    inputx = df.iloc[:,0:2]
+    inputy = df.iloc[0:,2]
+    print(inputx)
+    print(inputy)
 
-sns.scatterplot(x=df['Area'],y=df['Price'])
-plot.savefig(f'plt_area.png')
+    #Supervised learning
+    # split the data into train and test
+    input_train,input_test,output_train,output_test = train_test_split(inputx,inputy,test_size=1/4,random_state=0)
 
-sns.scatterplot(x=df['BedRooms'],y=df['Price'])
-plot.savefig(f'plt_br.png')
+    #instantiate a linear regression model
+    model = LinearRegression()
 
-inputx = df.iloc[:,0:2]
-inputy = df.iloc[0:,2]
-# print(inputy)
+    # train the regression model
+    model.fit(input_train,output_train)
+    pred_output = model.predict(input_test)
 
-#Supervised learning
-# split the data into train and test
-input_train,input_test,output_train,output_test = train_test_split(inputx,inputy,test_size=1/4,random_state=0)
+    # print to console the score pf the model
+    print(f"Auto calculated r2_score : ", model.score(input_test,output_test))
+    print(f"Manually calculated r2_score : ", r2_score(output_test,pred_output))
+    print(f"Mean Sqrd Error : ", root_mean_squared_error(output_test,pred_output))
+    print(f"model params coeff: { model.coef_}, intercept: {model.intercept_}")
 
-#create a model
-model = LinearRegression()
-
-# train the regression model
-model.fit(input_train,output_train)
-pred_output = model.predict(input_test)
-
-print(f"Auto calculated r2_score : ", model.score(input_test,output_test))
-print(f"Manually calculated r2_score : ", r2_score(output_test,pred_output))
-print(f"Mean Sqrd Error : ", root_mean_squared_error(output_test,pred_output))
-print(f"model params coeff: { model.coef_}, intercept: {model.intercept_}")
-
-#create a scaler object
-scaler = StandardScaler()
-
-#scale the inout train and test data sets
-input_scaled_train = scaler.fit_transform(input_train)
-input_scaled_test = scaler.transform(input_test)
-
-# train the model with scaling
-model.fit(input_scaled_train, output_train)
-pred_output2 = model.predict(input_scaled_test)
-
-print(f"Auto calculated r2_score with scaling : ", model.score(input_scaled_test,output_test))
-print(f"Manually calculated r2_score with scaling : ", r2_score(output_test,pred_output2))
-print(f"Mean Sqrd Error with scaling : ", root_mean_squared_error(output_test,pred_output2))
-print(f"model params coeff with scaling: { model.coef_}, intercept: {model.intercept_}")
-
-poly = PolynomialFeatures(degree=2, include_bias=False)
-input_train_poly = poly.fit_transform(input_train)
-input_test_poly = poly.transform(input_test)
-
-model_poly = LinearRegression()
-
-model_poly.fit(input_train_poly, output_train)
-pred_output_poly = model_poly.predict(input_test_poly)
-
-print(f"Auto calculated r2_score with poly: ", model_poly.score(input_test_poly,output_test))
-print(f"Manually calculated r2_score with poly : ", r2_score(output_test,pred_output_poly))
-print(f"Mean Sqrd Error with poly : ", root_mean_squared_error(output_test,pred_output_poly))
-print(f"model params coeff with poly: { model_poly.coef_}, intercept: {model_poly.intercept_}")
-
+    # configure matplotlib for saving to an image file
+    matplotlib.use('Agg')
+    # create an image of the plot
+    plot_3d_pred_homeprice(input_test['Area'],input_test['BedRooms'],pred_output)
 
 
 
