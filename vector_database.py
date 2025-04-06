4e5b0065bc37ab2f7a4ce2d4b0d806126ad61994
KumarGN69 <kumargn1969@hotmail.com>
2025-04-06 22:47:08 +0000

    vectorDB changes

---
+++ vector_database.py
@@ -0,0 +1,22 @@
+import chromadb, pprint
+
+vector = [1,2,3,4,5,6]
+print(f'vector :{vector}')
+
+client = chromadb.Client()
+
+collection = client.create_collection("all-my-documents")
+collection.add(
+    documents=[
+        "This is about food",
+        "This is about animal's food",
+        "This is about cats and dogs"
+        ],
+    metadatas = [{"topic1":"food"},{"topic2":"animal"},{"topic3":"animal"}],
+    ids=["doc1","doc2","doc3"],
+)
+results = collection.query(
+    query_texts = ["This is a query about food"],
+    n_results =2
+)
+pprint.pprint(results)
